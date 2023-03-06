328b783c6c081e60351efde50885fcfb143336f5
pratiksha123-gore <126436917+pratiksha123-gore@users.noreply.github.com>
2023-03-06 09:27:13 +0530

    Initial commit

---
+++ welcome_message EDUpath.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 id = "heading">EDUpath</h1>
+</body>
+</html>
+<style>
+  #heading {
+    left: 0;
+    line-height: 200px;
+    margin-top: -100px;
+    position: absolute;
+    text-align: center;
+    top: 50%;
+    width: 100%;
+}
+</style>
